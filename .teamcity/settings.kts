572ffb4a06a1c40e098bf6d01cbb5c2973d6f226
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-10-05 18:20:03 +0200

    ci: first TeamCity DSL iteraction

---
+++ .teamcity/settings.kts
@@ -0,0 +1,43 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+version = "2019.2"
+
+project {
+    vcsRoot(ObltVcs)
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+    artifactRules = "target/*jar"
+
+    vcs {
+        root(BuildConfVcs)
+    }
+    steps {
+        script {
+            scriptContent = "ls -la"
+        }
+    }
+    triggers {
+        vcs {
+            groupCheckinsByCommitter = true
+        }
+    }
+    requirements {
+      contains("teamcity.agent.name", "ubuntu-16")
+    }
+})
+
+object ObltVcs : GitVcsRoot({
+    name = "ObltVcs"
+    url = "https://github.com/kuisathaverat/teamCity-oblty.git"
+})
+
+object BuildConfVcs : GitVcsRoot({
+    name = "PetclinicVcs"
+    url = "https://github.com/elastic/apm-agent-python.git"
+})
