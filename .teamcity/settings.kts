4cafa454f1e90d7f22bfb69f15909f0ce6eb0fea
ivan.fernandez@elastic.co <ivan.fernandez@elastic.co@TeamCity>
2020-10-05 17:36:30 +0000

    TeamCity change in 'Observability' project: Synchronization with own VCS root is enabled

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,46 +1,62 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-version = "2019.2"
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.1"
 
 project {
-    vcsRoot(Observability_BuildConfVcs)
-    buildType(Build)
+
+    buildType(Basic)
+    buildType(BasicFromGit)
 }
 
-object Build : BuildType({
-    name = "Build"
-    artifactRules = "target/*jar"
+object Basic : BuildType({
+    name = "basic"
+    description = "basic test"
 
-    vcs {
-        root(BuildConfVcs)
-        checkoutMode = CheckoutMode.ON_AGENT
-        checkoutDir = "src"
-        cleanCheckout = true
-    }
     steps {
         script {
-            scriptContent = "ls -la"
+            name = "shell"
+            scriptContent = """echo "hello""""
         }
     }
+
+    requirements {
+        contains("teamcity.agent.name", "ubuntu-16")
+    }
+})
+
+object BasicFromGit : BuildType({
+    name = "basic from git"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
     triggers {
         vcs {
-            groupCheckinsByCommitter = true
         }
     }
-    requirements {
-      contains("teamcity.agent.name", "ubuntu-16")
-    }
-})
-
-object Observability_BuildConfVcs : GitVcsRoot({
-    name = "ObltVcs"
-    url = "https://github.com/kuisathaverat/teamCity-oblty.git"
-})
-
-object BuildConfVcs : GitVcsRoot({
-    name = "PetclinicVcs"
-    url = "https://github.com/elastic/apm-agent-python.git"
 })
