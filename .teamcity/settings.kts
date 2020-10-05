bb0168ef16528ca95cad9ab7a80f2d6b1a989901
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-10-05 19:50:56 +0200

    ci: add new build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,24 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -27,7 +45,6 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.1"
 
 project {
-
     buildType(Basic)
     buildType(BasicFromGit)
 }
@@ -49,14 +66,32 @@ object Basic : BuildType({
 })
 
 object BasicFromGit : BuildType({
-    name = "basic from git"
+    name = "Build"
+    artifactRules = "target/*jar"
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(BuildConfVcs)
+        checkoutMode = CheckoutMode.ON_AGENT
+        checkoutDir = "src"
+        cleanCheckout = true
     }
-
-    triggers {
-        vcs {
+    steps {
+        script {
+            scriptContent = "ls -la"
         }
     }
+    triggers {
+        vcs {
+            groupCheckinsByCommitter = true
+        }
+    }
+    requirements {
+        contains("teamcity.agent.name", "ubuntu-16")
+    }
 })
+
+object BuildConfVcs : GitVcsRoot({
+    name = "PetclinicVcs"
+    url = "https://github.com/elastic/apm-agent-python.git"
+})
+
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # teamCity-oblty
 Test Project for TemaCity
+
+* Create the Project in TeamCity UI
+    * Configure VCS Roots
+      * Add a new repo configuration
+    * Configure "Versioned Settings"
+      * Synchronization enabled
+      * use settings from VCS
+      * Push changes to the repo with **Commit current project settings…** button
+* Clone the repo locally
+* Edit the `.teamcity/settings.kts`
+  * add a new BuiltType
+  ```
+  import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+  
+  ...
+  
+  project {
+      buildType(MyTest)
+  }
+  
+  ...
+  
+  object MyTest : BuildType({
+      name = "Build"
+      steps {
+          script {
+              scriptContent = "echo 'Hello world'"
+          }
+      }
+  }
+  ```
+* Push the changes to the repo
+* Go
+
