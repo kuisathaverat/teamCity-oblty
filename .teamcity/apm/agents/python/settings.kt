6474ed03ca95568925db0309a09c36a58ba6c0f5
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-10-06 18:49:34 +0200

    ci: test subprojects

--- .teamcity/apm/agents/python/settings.kt
+++ .teamcity/apm/agents/python/settings.kt
@@ -23,7 +23,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 val operatingSystems = listOf("Mac OS X", "Windows", "Linux")
-val pythonVersions = listOf("2.7", "3.5", "3.7")
+val pythonVersions = listOf("2.7", "3.5", "3.7", "3.8")
 
 class ApmAgentPythonProject: Project ({
     id("apm_agent_python_project")
