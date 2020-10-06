9d062b242cbc3da4b2ea0505ee5ebae9e9b68377
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-10-06 18:43:00 +0200

    ci: test subprojects

--- .teamcity/apm/server/settings.kt
+++ .teamcity/apm/server/settings.kt
@@ -18,6 +18,8 @@
 package apm.server
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 class ApmServerProject: Project ({
@@ -25,6 +27,34 @@ class ApmServerProject: Project ({
     name = "APM Server"
 
     vcsRoot(ApmServerVcs)
+})
+
+class ApmServer() : BuildType ({
+    id("APM_server".toId())
+    name = "Agent Server"
+
+    vcs {
+        root(ApmServerVcs)
+        checkoutMode = CheckoutMode.ON_AGENT
+        checkoutDir = "src"
+        cleanCheckout = true
+    }
+
+    steps {
+        script {
+            scriptContent = """echo '${os} - ${version}'"""
+        }
+    }
+
+    triggers {
+        vcs {
+            groupCheckinsByCommitter = true
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "ubuntu-16")
+    }
 })
 
 object ApmServerVcs : GitVcsRoot({
