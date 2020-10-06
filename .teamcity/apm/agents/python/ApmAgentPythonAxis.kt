5987d5584aaf84d726f45595f8a4db64b0c4d1ea
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-10-06 20:12:37 +0200

    ci: test subprojects

---
+++ .teamcity/apm/agents/python/ApmAgentPythonAxis.kt
@@ -0,0 +1,52 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package apm.agents.python
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2019_2.toId
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+class ApmAgentPythonAxis(val os: String, val version: String) : BuildType({
+    id("APM_agent_Python_${os}_${version}".toId())
+    name = "Agent Python ${os} ${version}"
+
+    vcs {
+        root(ApmAgentPythonVcs)
+        checkoutMode = CheckoutMode.ON_AGENT
+        checkoutDir = "src"
+        cleanCheckout = true
+    }
+
+    steps {
+        script {
+            scriptContent = """echo '${os} - ${version}'"""
+        }
+    }
+
+    triggers {
+        vcs {
+            groupCheckinsByCommitter = true
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.name", "ubuntu-16")
+    }
+})
