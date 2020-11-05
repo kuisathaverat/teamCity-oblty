5c0a936b04a85fc595c710a99fa9d1d1eae43a71
kuisathaverat <kuisathaverat@gmail.com>
2020-11-05 22:10:28 +0100

    feat: add pool

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -85,7 +85,7 @@ project {
             param("preemptible", "false")
             param("sourceProject", "elastic-images-prod")
             param("sourceImageFamily", "elastic-kibana-ci-ubuntu-1804-lts")
-//            param("source-id", "elastic-kibana-ci-ubuntu-1804-lts")
+            param("source-id", "elastic-kibana-ci-ubuntu-1804-lts")
             param("zone", "us-central1-a")
             param("profileId", "Observability")
             param("diskType", "pd-ssd")
