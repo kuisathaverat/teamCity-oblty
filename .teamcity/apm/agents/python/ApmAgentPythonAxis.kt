809dda56a26c717526577b8d96cd1a6a6001096a
kuisathaverat <kuisathaverat@gmail.com>
2020-11-27 18:12:45 +0100

    test: python logic

--- .teamcity/apm/agents/python/ApmAgentPythonAxis.kt
+++ .teamcity/apm/agents/python/ApmAgentPythonAxis.kt
@@ -43,11 +43,10 @@ class ApmAgentPythonAxis(val os: String, val python: String) : BuildType({
     }
 
     steps {
-        frameworks.forEach { framework ->
-            script {
-                name = "Lint"
-                scriptContent = """
-                    echo '${os} - ${python} - ${framework}'
+        script {
+            name = "Lint"
+            scriptContent = """
+                    echo '${os} - ${python}'
                     export HOME=$(pwd)
                     export PATH=${'$'}{PATH}:${'$'}{HOME}/bin:${'$'}{HOME}/.ci/scripts:${'$'}{HOME}/.local/bin
                     alias python=python3
@@ -56,10 +55,12 @@ class ApmAgentPythonAxis(val os: String, val python: String) : BuildType({
                     pre-commit install --install-hooks
                     pre-commit run
                 """.trimIndent()
-            }
-            script {
-                name = "Test"
-                scriptContent = """
+        }
+        frameworks.forEach { framework ->
+            if(!excludes.contains("${python}:${framework}")){
+                script {
+                    name = "Test ${os} - ${python} - ${framework}"
+                    scriptContent = """
                     echo '${os} - ${python} - ${framework}'
                     export HOME=$(pwd)
                     export PATH=${'$'}{PATH}:${'$'}{HOME}/bin:${'$'}{HOME}/.ci/scripts:${'$'}{HOME}/.local/bin
@@ -68,6 +69,7 @@ class ApmAgentPythonAxis(val os: String, val python: String) : BuildType({
                     alias pip=pip3
                     ./tests/scripts/docker/run_tests.sh  ${python} ${framework}
                 """.trimIndent()
+                }
             }
         }
     }
